e9af91e7ad2c29d0b31151b868e3e0ef89196aa4
taliacohn <taliacohn3@gmail.com>
2023-03-02 11:46:19 +0200

    update session

--- session/session.js
+++ session/session.js
@@ -1,9 +1,8 @@
 const expressSession = require('express-session');
-const RedisStore = require('connect-redis')(expressSession);
+
 require('dotenv').config();
 
 const session = expressSession({
-  store: new RedisStore({ url: 'redis://localhost:6379' }),
   key: process.env.SESSION_KEY,
   secret: process.env.SESSION_SECRET,
   resave: false,
